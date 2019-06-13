0e7ecf26bccb2ff6c92a06c813ba27bae858cd78
rashadhuseyn <49521003+rashadhuseyn@users.noreply.github.com>
2019-06-13 12:55:23 +0300

    Add files via upload

--- shop/java2.js
+++ shop/java2.js
@@ -1,24 +1,69 @@
+'use strict';
 const buy = [...document.getElementsByClassName('butone')];
 const counter = document.querySelector('.counter');
 const body = document.querySelector('#body');
 const boughtProducds = JSON.parse(localStorage.getItem('basket'));
+const deleting = document.querySelector('.delete');
+
 if (localStorage.getItem("basket") === null) {
-    localStorage.setItem("basket", JSON.stringify([]));
+  localStorage.setItem("basket", JSON.stringify([]));
 }
-  else{
-    counter.innerText = boughtProducds.length;
-  };
-  
-  boughtProducds.forEach(element => {
-    const tr = document.createElement('tr')
-    body.appendChild(tr);
-    const td1 = document.createElement('td');
-    td1.innerText = element.name;
-    tr.appendChild(td1);
-    const td2 =document.createElement('td');
-    td2.innerText = element.price;
-    tr.appendChild(td2);
-    const td3 =document.createElement('td');
-    td3.innerText = element.id;
-    tr.appendChild(td3);
+else {
+  counter.innerText = boughtProducds.length;
+};
+let q = 0;
+boughtProducds.forEach(element => {
+  const tr = document.createElement('tr')
+  tr.style.cursor = 'pointer';
+  body.appendChild(tr);
+  const th = document.createElement('th');
+  th.innerText = `${q + 1}`
+  q++
+  tr.appendChild(th);
+  const td1 = document.createElement('td');
+  td1.innerText = element.name;
+  tr.appendChild(td1);
+  const td2 = document.createElement('td');
+  td2.innerText = element.price;
+  tr.appendChild(td2);
+  const td3 = document.createElement('td');
+  td3.innerText = element.id;
+  tr.appendChild(td3);
+  const img = document.createElement('img');
+  img.classList.add('small');
+  img.src = element.src;
+  tr.appendChild(img);
+  tr.addEventListener('click', function () {
+    tr.classList.toggle('selected');
+  })
+});
+
+deleting.addEventListener('click', () => {
+  document.querySelectorAll('.selected').forEach((item) => {
+    item.remove();
+    newLocalStorage();
   });
+});
+
+function newLocalStorage() {
+  localStorage.setItem("basket", JSON.stringify([]));
+  const basket = JSON.parse(localStorage.getItem('basket'));
+  const tr = [...document.getElementsByTagName('tr')];
+  tr.forEach(function (item) {
+    const proname = item.firstElementChild.nextSibling.innerText;
+    const proprice = item.firstElementChild.nextSibling.nextSibling.innerText;
+    const procode = item.lastElementChild.previousSibling.innerText;
+    const src = item.lastElementChild.src;
+    const product = {
+      id: procode,
+      name: proname,
+      price: proprice,
+      src: src
+    }
+    basket.push(product);
+    const newbasket = basket.slice(1);
+    localStorage.setItem("basket", JSON.stringify(newbasket));
+  })
+
+}
+
--- shop/main.js
+++ shop/main.js
@@ -1,12 +1,16 @@
+'use strict';
 const buy = [...document.getElementsByClassName('butone')];
 const counter = document.querySelector('.counter');
+const photos = [...document.getElementsByClassName('photo')];
 
 if (localStorage.getItem("basket") === null) {
     localStorage.setItem("basket", JSON.stringify([]));
   }
-
-
+  
+const bought = JSON.parse(localStorage.getItem("basket"));
+counter.innerText = bought.length;
 buy.forEach(bought=> {
+let i = buy.indexOf(bought);
 bought.onclick = function(e){
 e.preventDefault();
 const basket = JSON.parse(localStorage.getItem("basket")) ;
@@ -16,10 +20,12 @@ const proname = bought.getAttribute('proname');
 const product = {
 id: procode,
 name: proname,
-price: proprice    
+price: proprice + " AZN",
+src: photos[i].getAttribute('src')
 }
 basket.push(product);
 counter.innerText = basket.length;  
 localStorage.setItem("basket", JSON.stringify(basket));
+
 }
 }); 
